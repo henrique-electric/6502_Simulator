2b08668d1d5e848df31a4a6c37e17c3c72981e06
José Henrique <jhp.meira@gmail.com>
2024-07-27 23:52:06 -0300

    Continue to interpret more opcodes

--- src/cpu.c
+++ src/cpu.c
@@ -14,26 +14,47 @@ CPU init_new_cpu() {
     return new_cpu;
 }
 
-
+uint16_t cast_uint16_uint8_addrs(uint8_t high_bytes, uint8_t low_bytes) {
+    uint16_t fetched_addr = 0x0000;
+    fetched_addr = (fetched_addr & 0x00FF) | (high_bytes << 8);
+    fetched_addr = (fetched_addr & 0xFF00) | low_bytes;
+    return fetched_addr; 
+}
 void interpret_6502(CPU* cpu_ptr) {
     if (cpu_ptr != NULL) {
-        uint8_t opcode = 0x00;
-        uint8_t arg1 = 0x00;
-        uint8_t arg2 = 0x00;
-        uint8_t arg3 = 0x00;
-
+        uint8_t opcode          = 0x00;
+        uint8_t arg1            = 0x00;
+        uint8_t arg2            = 0x00;
+        uint8_t arg3            = 0x00;
+        uint16_t absolute_value = 0x0000;
         for (uint16_t count = 0; count < cpu_ptr->memory.bits_loaded_to_mem; count++) {
 
-            opcode = cpu_ptr->memory.virtual_memory[cpu_ptr->pc];
-            arg1 = cpu_ptr->memory.virtual_memory[cpu_ptr->pc + 1];
-            arg2 = cpu_ptr->memory.virtual_memory[cpu_ptr->pc + 2];
-            arg3 = cpu_ptr->memory.virtual_memory[cpu_ptr->pc + 3];
+            opcode          = cpu_ptr->memory.virtual_memory[cpu_ptr->pc];
+            arg1            = cpu_ptr->memory.virtual_memory[cpu_ptr->pc + 1];
+            arg2            = cpu_ptr->memory.virtual_memory[cpu_ptr->pc + 2];
+            arg3            = cpu_ptr->memory.virtual_memory[cpu_ptr->pc + 3];
+            absolute_value  = cast_uint16_uint8_addrs(arg1, arg2);
 
             switch (opcode) {
             case LDA_I:
                 cpu_ptr->register_a = arg1;
                 cpu_ptr->pc += 2;
                 cpu_ptr->cycle_count += 2;
+                break;
+
+            case LDA_AB:
+                cpu_ptr->register_a = memory_read_6502(cpu_ptr->memory, absolute_value);
+                cpu_ptr->pc += 2;
+                break;
+
+            case LDA_AX:
+                cpu_ptr->register_a = memory_read_6502(cpu_ptr->memory, absolute_value + (uint16_t) cpu_ptr->register_x);
+                cpu_ptr->pc += 2;
+                break;
+
+            case LDA_AY:
+                cpu_ptr->register_a = memory_read_6502(cpu_ptr->memory, absolute_value + (uint16_t) cpu_ptr->register_y);
+                cpu_ptr->pc += 2;
                 break;
 
             case LDX_I:
